cc385ad09f5f0c5d94457e1be88476e90ae95645
Burlacu Valeri <burlacu.valeri@gmail.com>
2018-07-17 00:05:30 +0300

    :

--- src/kyu6/ConvertStringToCamelCase.java
+++ src/kyu6/ConvertStringToCamelCase.java
@@ -3,10 +3,10 @@ Complete the method/function so that it converts dash/underscore
 delimited words into camel casing. The first word within the output
 should be capitalized only if the original word was capitalized.
 
-Examples
-toCamelCase("the-stealth-warrior"); // returns "theStealthWarrior"
+Examples:
+toCamelCase("the-stealth-warrior");  // returns "theStealthWarrior"
 
-toCamelCase("The_Stealth_Warrior"); // returns "TheStealthWarrior"
+toCamelCase("The_Stealth_Warrior");  // returns "TheStealthWarrior"
  */
 package kyu6;
 
